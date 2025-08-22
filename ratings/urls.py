37001ef46df9f8f955ac103370925cd0003d3833
Shivam Garg <gargshivam34has@gmail.com>
2025-08-22 22:24:45 +0530

    added telegram webhook v1

--- ratings/urls.py
+++ ratings/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import RoadRatingListCreate
+from .views import webhook
 
 urlpatterns = [
     path("ratings/", RoadRatingListCreate.as_view(), name="road-ratings"),
+    path("telegram-webhook/", webhook, name="telegram_webhook"),
 ]
--- ratings/views.py
+++ ratings/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import RoadRating
 from .serializers import RoadRatingSerializer
+from django.http import JsonResponse
+import requests, json
+import os
 
 class RoadRatingListCreate(generics.ListCreateAPIView):
 	queryset = RoadRating.objects.all().order_by("-created_at")
 	serializer_class = RoadRatingSerializer
+
+
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+TELEGRAM_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+
+def webhook(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+
+        # Reply back
+        reply = {"chat_id": chat_id, "text": f"You said: {text}"}
+        requests.post(TELEGRAM_URL, json=reply)
+
+        return JsonResponse({"status": "ok"})
+    return JsonResponse({"error": "invalid"}, status=400)
