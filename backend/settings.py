1790a467319a2735e87324cdde8a60548e6300bc
Shivam Garg <gargshivam34has@gmail.com>
2025-08-22 16:13:29 +0530

    added render.com in allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o*-qyh)!*vsjf8cc855(9__gjgz5ec@lz&+i!)i+infh9por-y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
