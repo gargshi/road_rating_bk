5816617d9106d855eb465659112d7fa7c1a7f8b3
Shivam Garg <gargshivam34has@gmail.com>
2025-08-26 22:11:21 +0530

    adding_models_to_admin_panel_v1

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-# from .models import RoadRating, UserConversation
+from .models import RoadRating, UserConversation
 # Register your models here.
-# class RoadRatingAdmin(admin.ModelAdmin):
-# 	list_display = ('road_name', 'rating', 'comment', 'created_at')
-# 	search_fields = ('road_name', 'comment')
-# 	list_filter = ('rating', 'created_at')
+class RoadRatingAdmin(admin.ModelAdmin):
+	list_display = ('road_name', 'rating', 'comment', 'created_at')
+	search_fields = ('road_name', 'comment')
+	list_filter = ('rating', 'created_at')
 
-# class UserConversationAdmin(admin.ModelAdmin):
-# 	list_display = ('chat_id', 'step', 'road_name', 'rating', 'comment', 'fk_road_id', 'updated_at')
-# 	search_fields = ('chat_id', 'road_name', 'comment')
-# 	list_filter = ('step', 'updated_at')
+class UserConversationAdmin(admin.ModelAdmin):
+	list_display = ('chat_id', 'step', 'road_name', 'rating', 'comment', 'fk_road_id', 'updated_at')
+	search_fields = ('chat_id', 'road_name', 'comment')
+	list_filter = ('step', 'updated_at')
 
-# admin.site.register(RoadRating, RoadRatingAdmin)
-# admin.site.register(UserConversation, UserConversationAdmin)
+admin.site.register(RoadRating, RoadRatingAdmin)
+admin.site.register(UserConversation, UserConversationAdmin)
