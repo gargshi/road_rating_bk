0d6841a8650e58410781cbe2da8b12f6f22f75e6
Shivam Garg <gargshivam34has@gmail.com>
2025-09-04 17:24:47 +0530

    display-data-v1

--- users_app/views.py
+++ users_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from ratings.models import RoadRating
 
 # Create your views here.
 def index(request):
-	return render(request, 'users_app/index.html')
+	#fetch ratings from db and pass to template
+	all_ratings = []  # Replace with actual query to fetch ratings
+	all_ratings = RoadRating.objects.all().order_by('-created_at')[:10]
+	context = {"ratings": all_ratings}
+	return render(request, 'users_app/index.html', context)
