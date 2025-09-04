26f97d94b13e462b3b5930c461e3605a41b55b17
Shivam Garg <gargshivam34has@gmail.com>
2025-09-04 17:59:02 +0530

    display-data-v3

--- users_app/views.py
+++ users_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from ratings.models import RoadRating
+from ratings.models import RoadRating, UserConversations
 
 # Create your views here.
 def index(request):
 	#fetch ratings from db and pass to template
 	all_ratings = []  # Replace with actual query to fetch ratings
-	all_ratings = RoadRating.objects.all().order_by('-created_at')[:10]
-	context = {"ratings": all_ratings}
+	user_conversations = []  # Replace with actual query to fetch user conversations
+	user_conversations = UserConversations.objects.all().order_by('-updated_at')[:10]
+
+	# all_ratings = RoadRating.objects.all().order_by('-created_at')[:10]
+
+	context = {"ratings": all_ratings, "user_conversations": user_conversations}
 	return render(request, 'users_app/index.html', context)
