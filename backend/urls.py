41bbd8f27af165a3cecb869fcf18d67882adab9d
Shivam Garg <gargshivam34has@gmail.com>
2025-09-04 14:54:24 +0530

    added-user-app-v2

--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("ratings.urls")),
-    path("", include("users_app.urls")),
+    path("/", include("users_app.urls")),
 ]
