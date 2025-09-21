0c392c85b88d369a860d4f6530148be926d697b3
Shivam Garg <gargshivam34has@gmail.com>
2025-09-21 19:22:54 +0530

    humanizing-v2

--- backend/settings.py
+++ backend/settings.py
@@ -62,6 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.locale.LocaleMiddleware',      # optional, for translations
+    'django.middleware.timezone.TimeZoneMiddleware',  # required for per-user timezones
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
