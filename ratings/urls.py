10b582c3bbea5689bb1841ff5bfb80a5a6ed1876
Shivam Garg <gargshivam34has@gmail.com>
2025-09-25 15:50:19 +0530

    some-ui-logic-changes-v1a

--- ratings/urls.py
+++ ratings/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views import RoadRatingListCreate,UserConversationListCreate
 from .views import webhook_widgets
 
 urlpatterns = [
-    path("ratings/", RoadRatingListCreate.as_view(), name="road-ratings"),
-	path("conversations/", UserConversationListCreate.as_view(), name="user-conversations"),
 	path("telegram-webhook/", webhook_widgets, name="telegram_webhook"),
 ]
